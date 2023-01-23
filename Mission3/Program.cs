1ce10012a3d9df0f6aa2e2aca438d7d553fb877f
Connor Chytraus <connorchyt@gmail.com>
2023-01-23 12:36:41 -0700

    Add project files.

---
+++ Mission3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Mission3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello User, welcome to Tic-Tac-Toe!");
+            Console.WriteLine("");
+
+            //create array to store choices
+            string[] choices = new string[9];
+            for (int i = 0; i < 9; i++)
+            {
+                choices[i] = Convert.ToString(i + 1);
+            }
+
+            supporting support = new supporting(choices);
+
+            int winFlag = -1;
+            int tieFlag = 0;
+
+            while (winFlag == -1 && tieFlag < 9)
+            {
+                //these are the spots that player 1 and 2 chose to play their turn
+                int place1 = 0;
+                int place2 = 0;
+                
+
+                Console.WriteLine("Player 1: X's Player 2: O's");
+
+                support.printBoard(choices);
+                Console.WriteLine("");
+                //print board here calling supporting class
+
+                Console.Write("Player 1's turn. Please select a space to play, then press enter: ");
+                place1 = Convert.ToInt32(Console.ReadLine());
+                choices[place1-1] = "X";
+                tieFlag++;
+
+                support.printBoard(choices);
+                Console.WriteLine("");
+                if (support.checkWinner(choices) != -1)
+                {
+                    winFlag = 1;
+                    break;
+                }
+
+                if (tieFlag == 9)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Player 1: X's Player 2: O's");
+
+                //print board here calling supporting class
+
+                Console.Write("Player 2's turn. Please select a space to play, then press enter: ");
+                place2 = Convert.ToInt32(Console.ReadLine());
+                choices[place2 - 1] = "O";
+                tieFlag++;
+                
+
+                //send place to support
+                //check win
+                
+                winFlag = support.checkWinner(choices);
+               
+            }
+
+            if (winFlag == 1 || winFlag == 2)
+            {
+                Console.WriteLine("Congratulations! Player " + winFlag + " won the game!");
+            }
+            else
+            {
+                Console.WriteLine("There was a tie");
+            }
+        }
+    }
+}
---
+++ Mission3/supporting.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Mission3
+{
+    public class supporting
+    {
+
+        private string[] board = new string[9];
+
+        public supporting(string[] choices)
+        {
+            board = choices;
+        }
+
+        public void printBoard(string[] choices)
+        {
+            Console.WriteLine(choices[0] + "  " + choices[1] + "  " + choices[2]);
+            Console.WriteLine(choices[3] + "  " + choices[4] + "  " + choices[5]);
+            Console.WriteLine(choices[6] + "  " + choices[7] + "  " + choices[8]);
+        }
+
+        public int checkWinner(string[] choices)
+        {
+            //horizontal Xs
+            if ("X" == choices[0] && "X" == choices[1] && "X" == choices[2])
+            {
+                return 1;
+            }
+            else if ("X" == choices[3] && "X" == choices[4] && "X" == choices[5])
+            {
+                return 1;
+            }
+            else if ("X" == choices[6] && "X" == choices[7] && "X" == choices[8])
+            {
+                return 1;
+            }
+
+            //horizotal Os
+            else if ("O" == choices[0] && "O" == choices[1] && "O" == choices[2])
+            {
+                return 2;
+            }
+
+            else if ("O" == choices[3] && "O" == choices[4] && "O" == choices[5])
+            {
+                return 2;
+            }
+
+            else if ("O" == choices[6] && "O" == choices[7] && "O" == choices[8])
+            {
+                return 2;
+            }
+
+            //Vertical Xs
+            else if ("X" == choices[0] && "X" == choices[3] && "X" == choices[6])
+            {
+                return 1;
+            }
+
+            else if ("X" == choices[1] && "X" == choices[4] && "X" == choices[7])
+            {
+                return 1;
+            }
+
+            else if ("X" == choices[2] && "X" == choices[5] && "X" == choices[8])
+            {
+                return 1;
+            }
+
+            // vertical O's
+            else if ("O" == choices[0] && "O" == choices[3] && "O" == choices[6])
+            {
+                return 2;
+            }
+
+            else if ("O" == choices[1] && "O" == choices[4] && "O" == choices[7])
+            {
+                return 2;
+            }
+
+            else if ("O" == choices[2] && "O" == choices[5] && "O" == choices[8])
+            {
+                return 2;
+            }
+
+            // diagonal O's
+            else if ("O" == choices[0] && "O" == choices[4] && "O" == choices[8])
+            {
+                return 2;
+            }
+
+            else if ("O" == choices[2] && "O" == choices[4] && "O" == choices[6])
+            {
+                return 2;
+            }
+
+            // diagonal X's
+            else if ("X" == choices[0] && "X" == choices[4] && "X" == choices[8])
+            {
+                return 1;
+            }
+
+            else if ("X" == choices[2] && "X" == choices[4] && "X" == choices[6])
+            {
+                return 1;
+            }
+
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
